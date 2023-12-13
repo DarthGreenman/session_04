3493d9401b41166fac7f1a83320cbeaff2e64a0c
Дмитрий Анатольевич Костюченко <141655594+DarthGreenman@users.noreply.github.com>
2023-12-13 11:59:26 +0300

    Added solution to homework Verification of basic functions of a two-biased list

---
+++ homework_04_04/homework_04_04_01/homework_04_04_01/homework_04_04_01.cpp
@@ -0,0 +1,43 @@
+﻿// homework_04_04_01.cpp: определяет точку входа для приложения.
+//
+
+#include "homework_04_04_01.h"
+#include "list.h"
+
+TEST_CASE("Testing methods of the netology::List class", "[methods: Empty(), Size(), Clear()]")
+{
+	netology::List list;
+
+	SECTION("Testing methods Empty()")
+	{
+		INFO("For an empty list, the Empty() method should return true, "
+			"otherwise it should return false");
+		CHECK(list.Empty() == true);
+		list.PushBack(100);
+		CHECK(list.Empty() == false);
+	}
+
+	SECTION("Testing methods Size()")
+	{
+		INFO("For an empty list, the Size() method should return 0, "
+			"otherwise it should return a value greater than 0");
+		CHECK(list.Size() == 0);
+		list.PushBack(100);
+		CHECK(list.Size() == 1);
+	}
+
+	SECTION("Testing methods Clear()")
+	{
+		list.PushBack(100);
+		list.Clear();
+		INFO("After calling the Clear() method, "
+			"the Empty() method should return true and Size() should return 0");
+		CHECK(list.Empty() == true);
+		CHECK(list.Size() == 0);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
---
+++ homework_04_04/homework_04_04_01/homework_04_04_01/homework_04_04_01.h
@@ -0,0 +1,9 @@
+/* homework_04_04_01.h */
+
+#ifndef HOMEWORK_04_04_01
+#define HOMEWORK_04_04_01
+
+#include "catch2/catch_test_macros.hpp"
+#include "catch2/catch_session.hpp"
+
+#endif /* HOMEWORK_04_04_01 */
